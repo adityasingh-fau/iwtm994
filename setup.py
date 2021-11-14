f0409e2010c330afa67ea11c19ea74a049711f38
adityasingh-fau <80420834+adityasingh-fau@users.noreply.github.com>
2021-11-14 20:07:11 +0100

    Version update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from setuptools import find_packages
 setup(name='iwtm994',
-version='0.1.2',
+version='0.1.3',
 author='DSSS',
 author_email='aditya.singh@fau.de',
 packages=find_packages(),
