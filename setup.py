41366ad361e2134c25d9b78dfc4b07e031b98340
adityasingh-fau <80420834+adityasingh-fau@users.noreply.github.com>
2021-11-14 20:32:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 from setuptools import find_packages
-setup(name='iwtm994',
-version='0.1.4',
+setup(name='za28tegu',
+version='0.1.5',
 author='DSSS',
 author_email='aditya.singh@fau.de',
 packages=find_packages(),
-install_requires=['numpy', 'matplotlib', 'imageio'])
+install_requires=['numpy', 'matplotlib'])
