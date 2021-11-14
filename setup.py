e96618aded14b495862c1b12df0a76e992d1a376
adityasingh-fau <80420834+adityasingh-fau@users.noreply.github.com>
2021-11-14 20:03:22 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 from setuptools import find_packages
 setup(name='iwtm994',
-version='0.1.1',
+version='0.1.2',
 author='DSSS',
 author_email='aditya.singh@fau.de',
 packages=find_packages(),
-install_requires=['numpy', 'Pillow', 'ipywidgets', 'opencv-python'])
+install_requires=['numpy', 'matplotlib', 'imageio'])
